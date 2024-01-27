f3c7c0d4c79dc6afc2b15b0a14541e478cf3193e
Soiman Al Fath <117827510+soimalfath@users.noreply.github.com>
2024-01-27 20:54:27 +0700

    fix grid cols

--- src/components/home/Feed.tsx
+++ src/components/home/Feed.tsx
@@ -9,7 +9,7 @@ const Feed = () => {
   };
   return (
     <section>
-      <div className={`grid grid-cols-${tabFeed.length} mt-3 border-b border-b-secondary`}>
+      <div className={`grid grid-cols-3 mt-3 border-b border-b-secondary`}>
         {tabFeed.map((tab, index) => (
           <div onClick={() => handleTabClick(tab.value)} className={`flex px-2 cursor-pointer md:hover:border md:hover:bg-slate-700 md:hover:border-slate-700 !pb-0 p-1 md:py-3 justify-center items-center`} key={index}>
             <div className={`${activeTab === tab.value ? "border-b-[#d59406] border-b-4 font-semibold text-white" : ""} pb-3 `}>{tab.tabName}</div>
