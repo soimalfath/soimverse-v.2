1b5801d1f71a64fd2c949f8f3309761b62c9814b
Soiman Al Fath <117827510+soimalfath@users.noreply.github.com>
2024-01-27 21:17:06 +0700

    fix border bottom feed

--- src/components/home/Feed.tsx
+++ src/components/home/Feed.tsx
@@ -9,10 +9,10 @@ const Feed = () => {
   };
   return (
     <section>
-      <div className={`grid grid-cols-3 mt-3 border-b border-b-secondary`}>
+      <div className={`grid grid-cols-3 mt-3 border-b-[0.1px] border-b-gray-700`}>
         {tabFeed.map((tab, index) => (
           <div onClick={() => handleTabClick(tab.value)} className={`flex px-2 cursor-pointer md:hover:border md:hover:bg-slate-700 md:hover:border-slate-700 !pb-0 p-1 md:py-3 justify-center items-center`} key={index}>
-            <div className={`${activeTab === tab.value ? "border-b-[#d59406] border-b-4 font-semibold text-white" : ""} pb-3 `}>{tab.tabName}</div>
+            <div className={`${activeTab === tab.value ? "border-b-[#d59406] rounded-sm border-b-4 font-semibold text-white" : ""} pb-2 `}>{tab.tabName}</div>
           </div>
         ))}
       </div>
