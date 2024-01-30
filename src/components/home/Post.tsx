fdcb64931a833c059b3c0a1e8206ba9a93257422
Soiman Al Fath <117827510+soimalfath@users.noreply.github.com>
2024-01-30 23:14:56 +0700

    add post

--- src/components/home/Post.tsx
+++ src/components/home/Post.tsx
@@ -2,14 +2,10 @@ import { profileImage } from "../../helper/feed";
 import { MdVerified } from "react-icons/md";
 import { TbPointFilled } from "react-icons/tb";
 import { post } from "../../common/interface";
-const post = ({
-  post,
-  image,
-  date
-}: post) => {
+const post = ({ post, image, date }: post) => {
   return (
     <section className="max-w-[900px]">
-      <div className="p-2 pr-4 border-b-[0.1px] border-b-gray-700">
+      <div className="p-2 pr-4 border-b-[0.1px] border-b-gray-700 mb-1">
         <div className="flex gap-4 pb-2">
           <div className="avatar">
             <div className="w-12 h-12 rounded-full">
@@ -23,11 +19,11 @@ const post = ({
               <div className="text-[#8899A6]">@mbuhsapaaa</div>
               <div className="text-[#8899A6] flex items-center ml-1 md:ml-0">
                 <TbPointFilled className="text-xs" />
-               {date}
+                {date}
               </div>
             </div>
             <div className="text-white">
-              <div className="mb-1">{post}</div>
+              <div className="mb-2">{post}</div>
               <div className="max-w-[800px]">
                 <img className="rounded-xl w-full" src={image} alt="" />
               </div>
--- src/helper/post.ts
+++ src/helper/post.ts
@@ -8,16 +8,21 @@ export const post = [
 
 export const about = [
     {
-        post: 'I am is a Frontend Developer and Digital Marketing Expert. I specialize in using Vue.js, Next.js, Tailwind, Bootstrap, Ant Design, and Node.js for my development work. In the world of digital marketing, I have over five years of experience and have worked as a Talent Lead for three years.',
-        date: '1 Jan 2019',
+        post: 'Hello, Im Soim, a seasoned professional with over six years of expertise in digital marketing and one year in frontend development. Specializing in crafting visually appealing interfaces using technologies such as Vue.js, Next.js, Tailwind, Bootstrap, Ant Design, and Node.js, I bring a comprehensive skill set to every project. My digital marketing proficiency spans SEO, social media, and analytics, and my passion for financial management ensures a strategic and holistic approach to project execution. With a proven track record of successful campaigns and a commitment to continuous learning, I strive to deliver high-quality work that exceeds expectations in both frontend development and digital marketing. Explore my portfolio to witness the convergence of technical expertise, marketing acumen, and financial insight in action. Lets collaborate to elevate your digital projects!',
+        date: '18 Jul 2018',
         image: 'https://www.qidigi.com/_next/image/?url=https%3A%2F%2Fveilannastore.com%2Fwp-content%2Fuploads%2F2023%2F04%2FFB_IMG_1569462794928.jpg&w=384&q=75'
     }
 ]
 
 export const project = [
     {
-        post : 'Qidigi App, qidigi apps is simple web app for digital marketer, they can manage daily report and finance report also add user for customer service, advertiser and admin. I create this web app use Vue.js 3 for Frontend and Firebase for Bcakend, you can try acces in "https://app.qidigi.com" ',
-        date: '1 Feb 2022',
-        image: 'https://veilannastore.com/wp-content/uploads/2024/01/e0335443-5ab5-4b57-b067-7ee03a23a0ef.jpg'
+        post : 'Qidigi App, qidigi apps is simple web app for digital marketer, they can manage dailly report and finance report also add user for customer service, advertiser and admin. I create this web app use Vue.js 3 with pinia and composition API  for Frontend and Firebase for Bcakend, you can try acces in "https://app.qidigi.com" ',
+        date: '1 Okt 2023',
+        image: 'https://veilannastore.com/wp-content/uploads/2024/01/Screenshot-2024-01-30-224755.png'
+    },
+    {
+        post : 'Komplace.id, Komplace is web app omnichanel marketplace for manage any store from anu marketplae in one place, komplace build with Vue.js 3 with pinia and composition API, you can try acces in "https://komplace.id" ',
+        date: '1 Jul 2023',
+        image: 'https://veilannastore.com/wp-content/uploads/2024/01/Screenshot-2024-01-30-225313.png'
     }
 ]
