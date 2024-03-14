7fd6fa715ddc3ec0a75d039021b80579f549de88
soimkmpbg <soimkmpbg@gmail.com>
2024-03-14 13:46:16 +0700

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        src: resolve(__dirname, 'src/index.tsx')
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
