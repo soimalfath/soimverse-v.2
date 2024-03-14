b4baf1845ba1b4618a3a8d51c60b0fd1117e7b41
soimkmpbg <soimkmpbg@gmail.com>
2024-03-14 14:26:29 +0700

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        src: resolve(__dirname, 'src/index.tsx')
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    open: true
-  }
 })
